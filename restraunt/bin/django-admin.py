1db227cb7b285963d2b28aa9d7b600793bd9c15d
Rakesh P Gopal <rakesh@actmobile.com>
2018-12-19 12:25:35 +0530

    Added the virtual env and Django Boiler-plate code

---
+++ restraunt/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rakesh/git/learning/restraunt_dashboard/restraunt/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
